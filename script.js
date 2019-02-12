c5fee4fe480a4983d1c9450386259ccedc4523b9
MaharshiJ <Maharshipway@gmail.com>
2019-02-12 13:30:11 -0500

    Added project files

--- script.js
+++ script.js
@@ -34,20 +34,74 @@ function playRound(playerSelection, computerSelection){
         return -1;
     }
 }
-
-function game(){
-    let rounds = 5;
-    var playerSelection = prompt("Rock Paper or Scissors");
+//plays a single round of rock paper scissors
+function game(playerSelect){
     var status;
     var computerSelection = computerPlay();
-    for (i = 0; i < rounds; i++){
-        status = playRound(playerSelection, computerSelection);
-        if (status == 1){
-            console.log("You won");
-        }else if (status == 0){ 
-            console.log("You tied");
-        }else if (status == -1){
-            console.log("You lost");
-        }
+    status = playRound(playerSelection, computerSelection);
+    if (status == 1){
+        console.log("You won");
+        return "Win";
+    }else if (status == 0){ 
+        console.log("You tied");
+        return "Tie";
+    }else if (status == -1){
+        console.log("You lost");
+        return "Lost";
     }
 }
+//creates a new element
+const statement = document.createElement('div');
+
+function displayResult(playerSelect) {
+    var gameStats = game(playerSelect);
+    //Assigns text to element based on final status of game
+    if (gameStats == "Win"){
+        statement.textContent = "YOU WON!";
+        statement.style.color = 'white';
+        statement.style.marginTop = '50px';
+        statement.style.fontSize = '50px';
+        statement.style.textAlign = 'center';
+        document.getElementById("display-statement").appendChild(statement);
+    }else if (gameStats == "Tie"){
+        statement.textContent = "YOU TIED";
+        statement.style.color = 'white';
+        statement.style.marginTop = '50px';
+        statement.style.fontSize = '50px';
+        statement.style.textAlign = 'center';
+        document.getElementById("display-statement").appendChild(statement);
+    }else if (gameStats == "Lost"){
+        statement.textContent = "YOU LOST :(";
+        statement.style.color = 'white';
+        statement.style.marginTop = '50px';
+        statement.style.fontSize = '50px';
+        statement.style.textAlign = 'center';
+        document.getElementById("display-statement").appendChild(statement);
+    }
+}
+
+
+
+var playerSelection = "";
+//reads rock button
+var rock = document.getElementById("rock");
+rock.addEventListener('click', () => {
+    playerSelection = "Rock";
+    statement.textContent = "";
+    displayResult(playerSelection);
+})
+//reads paper button
+var paper = document.getElementById("paper");
+paper.addEventListener('click', () => {
+    playerSelection = "Paper";
+    statement.textContent = "";
+    displayResult(playerSelection);
+})
+//reads scissors button
+var scissors = document.getElementById("scissors");
+scissors.addEventListener('click', () => {
+    playerSelection = "Scissors";
+    statement.textContent = "";
+    displayResult(playerSelection);
+})
+
